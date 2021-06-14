0da0b7a188fb36ff4fadd0601a79fdb8c5afb42a
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-06-14 19:04:07 +0700

    update alert

--- src/pages/Information.js
+++ src/pages/Information.js
@@ -10,6 +10,7 @@ function Information() {
 	const [mahasiswaData, setMahasiswaData] = useState();
 
 	const [inputs, setInputs] = useState({});
+	const [alertShow, setAlertShow] = useState(true);
 
 	function handleSearch(e) {
 		e.preventDefault();
@@ -96,6 +97,30 @@ function Information() {
 								</div>
 							</form>
 						</div>
+						{alertShow && (
+							<div
+								className="alert alert-primary alert-dismissible fade show text-left"
+								role="alert"
+							>
+								<i className="fa fa-exclamation-triangle me-2"></i>Ada
+								Pertanyaan?{" "}
+								<strong>
+									<a
+										className="text-light"
+										href="https://www.instagram.com/p/CQE8BYihfwm"
+										target="_blank"
+										rel="noreferrer"
+									>
+										Klik Di sini
+									</a>
+								</strong>
+								<button
+									onClick={() => setAlertShow(!alertShow)}
+									type="button"
+									class="btn-close"
+								></button>
+							</div>
+						)}
 					</div>
 					{mahasiswaData && (
 						<div className="mb-4">
@@ -118,6 +143,12 @@ function Information() {
 										<th scope="row">Bidang KTTA</th>
 										<td>{mahasiswaData.bidang_ktta}</td>
 									</tr>
+									{mahasiswaData.judul_ktta && (
+										<tr>
+											<th scope="row">Judul KTTA</th>
+											<td>{mahasiswaData.judul_ktta}</td>
+										</tr>
+									)}
 									<tr>
 										<th scope="row">Status</th>
 										<td>{mahasiswaData.status_konfirmasi}</td>
@@ -146,119 +177,123 @@ function Information() {
 									</tr>
 								</tbody>
 							</table>
-							<strong>Format Pengumpulan</strong>
-							<table className="table table-striped">
-								<thead>
-									<tr>
-										<th>Jenis</th>
-										<th>Qty (PDF)</th>
-										<th>Link</th>
-										<th>Nama</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<th scope="row">Lembar Persetujuan</th>
-										<td>1 lembar</td>
-										<td>
-											<a
-												className="btn btn-primary btn-sm"
-												href="http://gg.gg/persetujuanKTTA21"
-												rel="noreferrer"
-												target="_blank"
-											>
-												<i className="fa fa-link"></i>
-											</a>
-										</td>
-										<td>
-											<button
-												className="btn btn-warning btn-sm"
-												onClick={() => handlePersetujuan()}
-											>
-												<i className="fa fa-copy"></i>
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">Turnitin</th>
-										<td>
-											1 berkas
-											<br />
-											<span className="text-muted">Isi BAB I-IV</span>
-										</td>
-										<td>
-											<a
-												className="btn btn-primary btn-sm"
-												href="https://perpustakaan.stan.ac.id/layanan-pindai-turnitin-ktta/"
-												rel="noreferrer"
-												target="_blank"
-											>
-												<i className="fa fa-link"></i>
-											</a>
-										</td>
-										<td>
-											<button
-												className="btn btn-warning btn-sm"
-												onClick={() => handleTurnitin()}
-											>
-												<i className="fa fa-copy"></i>
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">Dosen Penilai I</th>
-										<td>
-											1 berkas
-											<br />
-											<span className="text-muted">Keseluruhan</span>
-										</td>
-										<td>
-											<a
-												className="btn btn-primary btn-sm"
-												href={`http://${mahasiswaData.link_dosnil1}`}
-												rel="noreferrer"
-												target="_blank"
-											>
-												<i className="fa fa-link"></i>
-											</a>
-										</td>
-										<td>
-											<button
-												className="btn btn-warning btn-sm"
-												onClick={() => handleDosnil()}
-											>
-												<i className="fa fa-copy"></i>
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">Dosen Penilai II</th>
-										<td>
-											1 berkas
-											<br />
-											<span className="text-muted">Keseluruhan</span>
-										</td>
-										<td>
-											<a
-												className="btn btn-primary btn-sm"
-												href={`http://${mahasiswaData.link_dosnil2}`}
-												rel="noreferrer"
-												target="_blank"
-											>
-												<i className="fa fa-link"></i>
-											</a>
-										</td>
-										<td>
-											<button
-												className="btn btn-warning btn-sm"
-												onClick={() => handleDosnil()}
-											>
-												<i className="fa fa-copy"></i>
-											</button>
-										</td>
-									</tr>
-								</tbody>
-							</table>
+							{!mahasiswaData.judul_ktta && (
+								<div>
+									<strong>Format Pengumpulan</strong>
+									<table className="table table-striped">
+										<thead>
+											<tr>
+												<th>Jenis</th>
+												<th>Qty (PDF)</th>
+												<th>Link</th>
+												<th>Nama</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<th scope="row">Lembar Persetujuan</th>
+												<td>1 lembar</td>
+												<td>
+													<a
+														className="btn btn-primary btn-sm"
+														href="http://gg.gg/persetujuanKTTA21"
+														rel="noreferrer"
+														target="_blank"
+													>
+														<i className="fa fa-link"></i>
+													</a>
+												</td>
+												<td>
+													<button
+														className="btn btn-warning btn-sm"
+														onClick={() => handlePersetujuan()}
+													>
+														<i className="fa fa-copy"></i>
+													</button>
+												</td>
+											</tr>
+											<tr>
+												<th scope="row">Turnitin</th>
+												<td>
+													1 berkas
+													<br />
+													<span className="text-muted">Isi BAB I-IV</span>
+												</td>
+												<td>
+													<a
+														className="btn btn-primary btn-sm"
+														href="https://perpustakaan.stan.ac.id/layanan-pindai-turnitin-ktta/"
+														rel="noreferrer"
+														target="_blank"
+													>
+														<i className="fa fa-link"></i>
+													</a>
+												</td>
+												<td>
+													<button
+														className="btn btn-warning btn-sm"
+														onClick={() => handleTurnitin()}
+													>
+														<i className="fa fa-copy"></i>
+													</button>
+												</td>
+											</tr>
+											<tr>
+												<th scope="row">Dosen Penilai I</th>
+												<td>
+													1 berkas
+													<br />
+													<span className="text-muted">Keseluruhan</span>
+												</td>
+												<td>
+													<a
+														className="btn btn-primary btn-sm"
+														href={`http://${mahasiswaData.link_dosnil1}`}
+														rel="noreferrer"
+														target="_blank"
+													>
+														<i className="fa fa-link"></i>
+													</a>
+												</td>
+												<td>
+													<button
+														className="btn btn-warning btn-sm"
+														onClick={() => handleDosnil()}
+													>
+														<i className="fa fa-copy"></i>
+													</button>
+												</td>
+											</tr>
+											<tr>
+												<th scope="row">Dosen Penilai II</th>
+												<td>
+													1 berkas
+													<br />
+													<span className="text-muted">Keseluruhan</span>
+												</td>
+												<td>
+													<a
+														className="btn btn-primary btn-sm"
+														href={`http://${mahasiswaData.link_dosnil2}`}
+														rel="noreferrer"
+														target="_blank"
+													>
+														<i className="fa fa-link"></i>
+													</a>
+												</td>
+												<td>
+													<button
+														className="btn btn-warning btn-sm"
+														onClick={() => handleDosnil()}
+													>
+														<i className="fa fa-copy"></i>
+													</button>
+												</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							)}
 						</div>
 					)}
 					<div className="text-center mb-4">
