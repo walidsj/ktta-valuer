f179a63cd9a398c626351fd6dc6282158a23a9eb
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-06-16 17:42:14 +0700

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -40,7 +40,7 @@ function App() {
 					/>
 					<h2 className="mb-0 fw-bold">Live Count</h2>
 					<h4>Pengumpulan KTTA</h4>
-          <TimerCountdown />
+					<TimerCountdown />
 				</div>
 				<div className="mb-3">
 					<h1 className="d-inline-flex fw-bold">
@@ -66,7 +66,8 @@ function App() {
 				</div>
 				<div className="pt-2">
 					<Link to="/informasi" className="btn btn-warning">
-						<i className="fa fa-paste me-2" />Info Pengumpulan
+						<i className="fa fa-paste me-2" />
+						Info Pengumpulan
 					</Link>
 				</div>
 				<div className="pt-3">
@@ -76,7 +77,7 @@ function App() {
 								indicatorShow ? "fadeIn" : "fadeOut"
 							} fa fa-circle text-primary me-1`}
 							style={{ fontSize: "9px" }}
-						 />
+						/>
 						realtime (update/10s)
 					</small>
 					<br />
@@ -87,7 +88,8 @@ function App() {
 							target="_blank"
 							rel="noreferrer"
 						>
-							<i className="fa fa-instagram ms-2 me-1" />Ranger Valuer
+							<i className="fa fa-instagram ms-2 me-1" />
+							Ranger Valuer
 						</a>
 					</small>
 				</div>
