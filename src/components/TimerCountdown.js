e073bc1475c03deb447543446ad6bd49ed6e3a69
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2021-07-23 20:37:19 +0700

    update timer final

--- src/components/TimerCountdown.js
+++ src/components/TimerCountdown.js
@@ -17,16 +17,20 @@ function TimerCountdown() {
 
       const distanceToDate = countdownDate - currentTime;
 
-      let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
-      let hours = Math.floor(
-        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-      );
-      let minutes = Math.floor(
-        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
-      );
-      let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
+      if (distanceToDate < 0) {
+        setState({ days: "0", hours: "0", minutes: "0", seconds: "0" });
+      } else {
+        let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
+        let hours = Math.floor(
+          (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        let minutes = Math.floor(
+          (distanceToDate % (1000 * 60 * 60)) / (1000 * 60)
+        );
+        let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
 
-      setState({ days: days, hours: hours, minutes, seconds });
+        setState({ days, hours, minutes, seconds });
+      }
     }
   };
 
